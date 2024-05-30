d132ab7a217952d763994568f26cdf73eb86d65b
Rodrigo Bezerra <rodbn87@gmail.com>
2024-05-30 19:35:31 +0100

    Criando uma primeira versão da base da página

--- myapp/urls.py
+++ myapp/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib.auth import views
+from core.views import index, about
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name="index"),
+    path('about/', about, name="about"),
 ]
